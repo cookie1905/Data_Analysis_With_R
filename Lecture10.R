c57883c4dc9d0654f349b2fdd7f868860fd5f89d
cookie1905 <neethu51193@gmail.com>
2025-11-22 22:00:03 +0100

    new lecture

---
+++ Lecture10.R
@@ -0,0 +1,134 @@
+# R Lecture 10
+# Neethu Raj
+# Based on lecture series "Data analysis using R" by Prof. Danny Arends
+# ==========================================
+
+setwd("C:\\Data_Analysis_With_R")  # Set working directory
+
+# Logistic Regression 
+# When Response variable is not continuous (discrete, categorical)
+
+# Load in the data
+data <- read.csv("https://stats.idre.ucla.edu/stat/data/binary.csv")
+head(data)
+
+# response variable: admit (0 / 1)
+# predictor variables: gre, gpa and rank 
+
+unique(data$rank)            # 1 2 3 4
+
+# Contingency Table
+#  for each rank (1–4), the counts of admit = 0 and admit = 1.
+
+xtabs(~admit + rank, data = data)
+
+# If for any rank, count = 0 for admit == 0 or 1 then 
+# we cannot use that rank in logistic regression.
+# each predictor category needs variation in the outcome (both 0 and 1) to estimate an effect.
+
+# Note: need to do this when predictor is a category
+
+#
+levels(data$rank)                 # Not a factor
+data$rank <- as.factor(data$rank) # change to a factor
+
+levels(data$rank) 
+
+# Model the data
+?glm
+
+glm.model <- glm(formula = admit ~ gre + gpa + rank,
+    data = data, 
+    family = "binomial")   # response variable is binary (0/1)
+
+summary(glm.model)
+# gra and gpe are significant predictors
+# 
+# rank2       -0.675443   0.316490  -2.134 0.032829 *  
+# rank3       -1.340204   0.345306  -3.881 0.000104 ***
+# rank4       -1.551464   0.417832  -3.713 0.000205 ***
+#
+# Why no rank1?
+# rank1 is the reference category (since its the first level of rank)
+# because for ranks 2 3 and 4 the parameters are given relative to that of rank1 (since its the first level)
+#
+# rank2       -0.675443
+# means the log‑odds of admission for rank2 are 0.675 lower than rank1, holding other variables constant.
+# and that for rank 3 and 4 are even lower.
+# 
+# But how significant is the rank as a whole to determine admit?
+#
+install.packages("aod")
+library(aod)
+
+coef(glm.model) # or
+glm.model$coefficients
+
+glm.model$coefficients[c("rank2", "rank3", "rank4")]
+# or
+glm.model$coefficients[4:6]
+
+# Combine significance of  3 ranks into 1
+wald.test(b = glm.model$coefficients, 
+          Sigma = vcov(glm.model),   # variance–covariance matrix of the estimated coefficients of glm.model
+          Terms = 4:6)
+
+# After this we get a single p.value (0.00011) very significant
+# Chi-squared test:
+# X2 = 20.9, df = 3, P(> X2) = 0.00011
+
+# 
+# In linear regression, we report the coefficients directly.
+# Each coefficient shows how much the outcome changes when that predictor increases by one unit (holding others constant).
+# if coefficient = 2, then Y goes up by 2 for every +1 in X.
+
+
+# In logistic regression, we report odds ratio.
+# example: the chance of someone being admitted.
+
+# so instead of reporting
+glm.model$coefficients
+
+# we report
+exp(glm.model$coefficients)
+
+# gre 1.0022670
+# means: for every +1 increase in GRE,
+# the odds of admission are multiplied by 1.0022670
+
+# Percent change in odds = (Odds ratio − 1 ) × 100%
+# (1.0022670 − 1) × 100 = 0.2267%
+# So each +1 GRE point increases the odds of admission only by about 0.23%.
+
+# gpa   2.2345448
+# Higher gpa increases odds by approximately 123.45%.
+
+# Note
+# Percent change measures how much larger something becomes relative to the original.
+# If something doubles, 
+# Percent change = (2 - 1) * 100 = 100%
+# If it goes from 1 to 3, (3 - 1) * 100 = 200%
+
+# rank2   0.5089310
+# rank 2 decreases the chance of getting admitted by 50% (percent change in odds = −49.1069%)
+# rank3        0.2617923
+# and rank 3 decreases the chance by 73.82077%
+
+# To get a 95% Confidence Interval
+exp(cbind(odds.ratio = glm.model$coefficients, confint(glm.model)))
+
+
+# glm(...., family = "binomial"): if response variable is  binomial but 
+# it can be:
+# Gaussian
+# gamma
+# inverse.Gaussian
+# Poisson
+
+# glm(...., family = "gaussian") is exactly same as using lm()
+#
+# which distribution? depends on data
+# if we have count data then Poisson 
+
+
+
--- Lecture9.R
+++ Lecture9.R
@@ -83,14 +83,14 @@ ggplot(freq.data, aes(x = subject, y = frequency, fill = gender)) +
 # random intercept
 
 # As per the design they also asked each subject different questions
-# depending on question answers can be different
-# and can have different words and letters and the frequency then depend on these as well
+# depending on question attitude can be different
+# As a result, frequency of voice can also change.
 # per-item variation
 # 
 
 boxplot(frequency ~ scenario, data = freq.data,
         main = "Frequency by Scenario/Question",
-        xlab = "Subject",
+        xlab = "Scenario",
         ylab = "Frequency",
         cex.main = 0.8)
 
@@ -103,8 +103,19 @@ levels(freq.data$scenario) # is NULL
 # ggplot needs us to explicitly tell when a numeric variable should be treated as categorical.
 # Base r does this automatically
 
-ggplot(freq.data, aes(x = as.factor(scenario), y = frequency)) 
-  geom_boxplot() 
+ggplot(freq.data, aes(x = as.factor(scenario), y = frequency)) + 
+  geom_boxplot() + 
+  labs(title = "Frequency by Scenario",
+       x = "Scenario",
+       y = "Frequency")+
+  theme_minimal()
+
+
+boxplot(frequency ~ attitude, data = freq.data,
+        main = "Frequency by Attitude",
+        xlab = "Attitude",
+        ylab = "Frequency",
+        cex.main = 0.8)
 
 # Extending the Model again:
 # frequency ~ attitude + gender + (1|subject) + (1|item) + error term
@@ -112,3 +123,137 @@ ggplot(freq.data, aes(x = as.factor(scenario), y = frequency))
 # each individual is allowed to have its own intercept.
 # each question is also allowed to have its own intercept.
 # So we take into account variation within subjects and within questions.
+
+# LMM in R
+# lmer() in package lme4
+# similar to lm()
+
+install.packages("lme4")
+library(lme4)
+?lmer
+
+head(freq.data)
+
+boxplot(frequency ~ attitude + gender,
+        col = c("orange", "lightblue"),
+        data = freq.data)
+
+lmer(formula = frequency ~ attitude + gender,
+     data = freq.data)
+# Error: No random effects terms specified in formula
+# lmr() expects a LMM model so it needs at least 1 random effect.
+
+lmm.model <- lmer(formula = frequency ~ attitude + gender  + (1|subject) + (1|scenario),
+     data = freq.data)
+
+summary(lmm.model)
+
+# What we are interested in :-
+#
+# Fixed effects:
+# Estimate Std. Error t value
+# (Intercept)  256.846     16.116  15.938
+# attitudepol  -19.721      5.584  -3.532
+# genderM     -108.516     21.013  -5.164
+
+#1. attitudepol (-19.721)
+# If you are talking politely your voice will go down by 19Hz compared to when you speak informally. 
+# Or in other words
+# Speaking politely is associated with a ~19.7 Hz lower frequency compared to informal speech, holding gender constant.
+
+#2. genderM (-108.516)
+# males have a ~108.5 Hz lower frequency compared to females, holding attitude constant.
+
+# To view all 7 intercepts for each scenario and subject
+#
+ranef(lmm.model)
+
+# Compare 2 Models
+# Check Model Significance
+# ==========================================
+#
+# First model doesnot include factor of interest (no attitude)
+lmm.moel1 <- lmer(formula = frequency ~ gender  + (1|subject) + (1|scenario),
+                  data = freq.data,
+                  REML = FALSE)
+
+# Second model includes factor of interest (attitude)
+lmm.moel2 <- lmer(formula = frequency ~ attitude + gender  + (1|subject) + (1|scenario),
+                  data = freq.data,
+                  REML = FALSE)
+
+anova(lmm.moel1, lmm.moel2)
+
+# look at the AIC    
+# lmm.moel1   816.72
+# lmm.moel2   807.10
+
+# There is a difference of 10 between the models
+# lmm.model2 is better than lmm.model1
+
+# p ~ 0.0006532 ***
+# lmm.model2 fits significantly better than lmm.model1 
+
+# Above models are Random Intercept models
+# intercept is different for each subject and scenario, but
+# fixed effects are same for all subjects and scenarios
+
+# So the model accounted baselines differences in frequency but assume
+# the effects of attitude and gender on frequency is same for all subjects and scenarios. 
+
+# So in Random Intercept model, there are
+# Random intercepts: each subject and scenario has its own baseline starting frequency.
+# Fixed effects: the effect of attitude (≈ –19 Hz when polite) and gender (≈ –108 Hz for males compared to females) are constant across all subjects and scenarios.
+
+# Regardless of the speaker or the scenario, 
+# speaking politely leads to an average drop of about 19 Hz in voice frequency, 
+# and men’s voices are on average 108 Hz lower than women’s.
+
+# But this is a strong assumption
+# because it forces the effect of attitude and gender to be identical across all subjects and scenarios
+# Some people may be more polite than others
+# or react differently to same scenario 
+
+# A random slope model relaxes this by allowing the effect of a predictor (attitude or gender)
+# to vary across subjects or scenarios, not just the intercept.
+# So each subject/scenario can have its own baseline and its own strength of effect.
+# So different intercepts and different slopes
+
+#
+# Random Slope Model
+# ==========================================
+# 
+freq.model <- lmer(formula = frequency ~ attitude + gender  + 
+                     (1 + attitude | subject) + (1 + attitude | scenario),
+                   data = freq.data,
+                   REML = FALSE)
+# 
+# here we assume gender effect is constant 
+# but different slope for the effect of attitude across subjects and scenarios
+
+summary(freq.model)
+
+# Random effects:
+# Groups   Name        Variance Std.Dev. Corr
+# scenario (Intercept) 182.083  13.494       
+# attitudepol  31.244   5.590   0.22
+# subject  (Intercept) 392.344  19.808       
+# attitudepol   1.714   1.309   1.00
+
+# For scenarios, variance of attitude slope ≈ 31.2, ie
+# effect of attitude varies across scenarios.
+# For subjects, variance of attitude slope is only ≈ 1.7 
+# suggesting less variation across subjects.
+
+ranef(freq.model)        # see we now have different slopes as well
+
+# Note
+# The fixed effect for attitude is the average effect across all groups.
+# Random slopes show whether that effect differs across subjects/scenarios
+
+
+# Attitude (politeness) lowers frequency overall,#
+# Men’s voices are lower,
+# Size of the politeness effect (the drop in frequency) is not constant across scenarios.
+# Some scenarios show a stronger drop, others a weaker one.
+# Effect of attitude doesn't vary much across subjects. 
